a466acb6a0f9a03c4a6f98d55423a9553ff5cdf1
EvrkMs <evrk.msn@gmail.com>
2025-10-05 11:00:55 +0300

    del pk in auto index

--- ComputerAdminAuth/Auth.EntityFramework/Data/ModelBuilderExtensions.cs
+++ ComputerAdminAuth/Auth.EntityFramework/Data/ModelBuilderExtensions.cs
@@ -11,50 +11,19 @@ public static class ModelBuilderExtensions
     /// </summary>
     /// <param name="modelBuilder">EF ModelBuilder</param>
     /// <param name="indexMethod">Тип индекса: "btree" (по умолчанию) или "hash"</param>
-    public static void AddIndexesForKeys(this ModelBuilder modelBuilder, string? indexMethod = null)
+    public static void AddIndexesForForeignKeys(this ModelBuilder modelBuilder)
     {
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-            // --- Primary Key ---
-            var pk = entityType.FindPrimaryKey();
-            if (pk != null)
-            {
-                var pkProps = pk.Properties.Select(p => p.Name).ToArray();
-
-                // Проверим, есть ли уже индекс на PK
-                bool hasPkIndex = entityType
-                    .GetIndexes()
-                    .Any(i => i.Properties.Select(p => p.Name).SequenceEqual(pkProps));
-
-                if (!hasPkIndex)
-                {
-                    var indexBuilder = modelBuilder.Entity(entityType.ClrType)
-                        .HasIndex(pkProps)
-                        .HasDatabaseName($"IX_{entityType.GetTableName()}_PK");
-
-                    if (indexMethod != null)
-                        indexBuilder.HasMethod(indexMethod);
-                }
-            }
-
-            // --- Foreign Keys ---
             foreach (var fk in entityType.GetForeignKeys())
             {
-                var fkProps = fk.Properties.Select(p => p.Name).ToArray();
-
-                // Проверим, есть ли уже индекс на FK
-                bool hasFkIndex = entityType
-                    .GetIndexes()
-                    .Any(i => i.Properties.Select(p => p.Name).SequenceEqual(fkProps));
-
-                if (!hasFkIndex)
+                // Добавляем индекс только если FK участвует в каскадном удалении или явно используется
+                if (fk.DeleteBehavior != DeleteBehavior.NoAction)
                 {
-                    var indexBuilder = modelBuilder.Entity(entityType.ClrType)
-                        .HasIndex(fkProps)
-                        .HasDatabaseName($"IX_{entityType.GetTableName()}_{string.Join("_", fkProps)}");
-
-                    if (indexMethod != null)
-                        indexBuilder.HasMethod(indexMethod);
+                    var props = fk.Properties.Select(p => p.Name).ToArray();
+                    modelBuilder.Entity(entityType.ClrType)
+                        .HasIndex(props)
+                        .HasDatabaseName($"IX_{entityType.GetTableName()}_{string.Join("_", props)}");
                 }
             }
         }
